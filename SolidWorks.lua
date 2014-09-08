af3e6a4ac312b8462d9e38c775296f355ffe3d73
JakeChapeskie <jake.chapeskie@thalmic.com>
2014-09-08 16:12:18 -0400

    Fix SolidWorks with conditional roll check
    checks how the Myo is orientated for the zoom.

--- SolidWorks.lua
+++ SolidWorks.lua
@@ -9,27 +9,27 @@ scriptId = 'com.thalmic.solidworks'
 --hold fist and turn cw and ccw to zoom
 --LOAD SCRIPT BEFORE DOING SETUP GESTURE TO MAKE SURE XPOS WORKS
 
-PITCH_MOTION_THRESHOLD = 5 -- degrees
-YAW_MOTION_THRESHOLD = 5 -- degrees
-ROLL_MOTION_THRESHOLD = 5 -- degrees
+PITCH_MOTION_THRESHOLD = 7 -- degrees
+YAW_MOTION_THRESHOLD = 6 -- degrees
+ROLL_MOTION_THRESHOLD = 7 -- degrees
 SLOW_MOVE_PERIOD = 50
 
 --Helper Functions
 function getMyoYawDegrees()
-	local yawValue = math.deg(myo.getYaw())
-	return yawValue
+    local yawValue = math.deg(myo.getYaw())
+    return yawValue
 end
 function getMyoPitchDegrees()
-	local PitchValue = math.deg(myo.getPitch())
-	return PitchValue
+    local PitchValue = math.deg(myo.getPitch())
+    return PitchValue
 end
 function getMyoRollDegrees()
-	local RollValue = math.deg(myo.getRoll())
-	return RollValue
+    local RollValue = math.deg(myo.getRoll())
+    return RollValue
 end
 function degreeDiff(value, base)
     local diff = value - base
-	if diff > 180 then
+    if diff > 180 then
         diff = diff - 360
     elseif diff < -180 then
         diff = diff + 360
@@ -37,11 +37,18 @@ function degreeDiff(value, base)
     return diff
 end
 function conditionalPitch(pitch)
-	if myo.getXDirection()== "towardElbow" then
-		pitch=-pitch;
-	end
-	return pitch
+    if myo.getXDirection()== "towardElbow" then
+        pitch=-pitch;
+    end
+    return pitch
 end
+function conditionalRoll(roll)
+    if myo.getXDirection()== "towardElbow" then
+        roll=-roll;
+    end
+    return roll
+end
+
 function conditionallySwapWave(pose)
     if myo.getArm() == "left" then
         if pose == "waveIn" then
@@ -55,36 +62,36 @@ end
 
 --Control Functions
 function zoomIn()
-	myo.keyboard("z", "press","shift")
+    myo.keyboard("z", "press","shift")
 end
 function zoomOut()
-	myo.keyboard("z", "press")
+    myo.keyboard("z", "press")
 end
 function moveLeft()
-	myo.keyboard("left_arrow", "press")
+    myo.keyboard("left_arrow", "press")
 end
 function moveRight()
-	myo.keyboard("right_arrow", "press")
+    myo.keyboard("right_arrow", "press")
 end
 function moveUp()
-	myo.keyboard("up_arrow", "press")
+    myo.keyboard("up_arrow", "press")
 end
 function moveDown()
-	myo.keyboard("down_arrow", "press")
+    myo.keyboard("down_arrow", "press")
 end
 
 function resetToTop()
-	myo.keyboard("5", "press","control")
+    myo.keyboard("5", "press","control")
 end
 function resetToFront()
-	myo.keyboard("1", "press","control")
+    myo.keyboard("1", "press","control")
 end
 function resetToBack()
-	myo.keyboard("2", "press","control")
+    myo.keyboard("2", "press","control")
 end
 --Not Used
 function resetToIso()
-	myo.keyboard("7", "press","control")
+    myo.keyboard("7", "press","control")
 end
 --Toggle Unlock Functions
 function lock()
@@ -99,84 +106,85 @@ function unlock()
 end
 
 function onPoseEdge(pose, edge)
-	pose=conditionallySwapWave(pose)
-	local now = myo.getTimeMilliseconds()
-	--Hold to move activation
-	if pose == "fist" and enabled then
+    pose=conditionallySwapWave(pose)
+    local now = myo.getTimeMilliseconds()
+    --Hold to move activation
+    if pose == "fist" and enabled then
         moveActive = edge == "on"
         yawReference = getMyoYawDegrees()
-		pitchReference = getMyoPitchDegrees()
-		rollReference = getMyoRollDegrees()
+        pitchReference = getMyoPitchDegrees()
+        rollReference = getMyoRollDegrees()
         moveSince = now
-   	end
-	--Other shortcut control
-	if edge == "on" then
-		if pose == "waveIn" and enabled then
-			resetToFront()
-		elseif pose == "waveOut" and enabled then
-			resetToBack()
-		elseif pose == "fingersSpread" and enabled  then
-			resetToTop() 
-		elseif pose == "thumbToPinky" then
-			if enabled then
+       end
+    --Other shortcut control
+    if edge == "on" then
+        if pose == "waveIn" and enabled then
+            resetToFront()
+        elseif pose == "waveOut" and enabled then
+            resetToBack()
+        elseif pose == "fingersSpread" and enabled  then
+            resetToTop() 
+        elseif pose == "thumbToPinky" then
+            if enabled then
                 lock()
             else
                 unlock()
             end
-		end
-	end
+        end
+    end
 end
 
 -- onPeriodic runs every ~10ms
 function onPeriodic()
-	local now = myo.getTimeMilliseconds()
-	if moveActive then
+    local now = myo.getTimeMilliseconds()
+    if moveActive then
         local relativeYaw = degreeDiff(getMyoYawDegrees(), yawReference)
-		if math.abs(relativeYaw)> YAW_MOTION_THRESHOLD then
-			if now - moveSince > SLOW_MOVE_PERIOD then
-				if relativeYaw>0 then
-					moveLeft()
-				else
-					moveRight()
-				end	
-				moveSince = now
-			end
-		end
-		local relativePitch = degreeDiff(getMyoPitchDegrees(), pitchReference)
-		relativePitch=conditionalPitch(relativePitch)
-		if math.abs(relativePitch)> PITCH_MOTION_THRESHOLD then
-			if now - moveSince > SLOW_MOVE_PERIOD then
-				if myo.getXDirection()== "towardElbow" then
-					relativePitch=-relativePitch;
-				end
-				if relativePitch>0 then
-					moveDown()
-				else
-					moveUp()
-				end	
-				moveSince = now
-			end
-		end
-		
-		local relativeRoll = degreeDiff(getMyoRollDegrees(), rollReference)
-		if math.abs(relativeRoll)> ROLL_MOTION_THRESHOLD then
-			if now - moveSince > SLOW_MOVE_PERIOD then
-				if relativeRoll>0 then
-					zoomIn()
-				else
-					zoomOut()
-				end	
-				moveSince = now
-			end
-		end
-		
-	end
+        if math.abs(relativeYaw)> YAW_MOTION_THRESHOLD then
+            if now - moveSince > SLOW_MOVE_PERIOD then
+                if relativeYaw>0 then
+                    moveLeft()
+                else
+                    moveRight()
+                end    
+                moveSince = now
+            end
+        end
+        local relativePitch = degreeDiff(getMyoPitchDegrees(), pitchReference)
+        relativePitch=conditionalPitch(relativePitch)
+        if math.abs(relativePitch)> PITCH_MOTION_THRESHOLD then
+            if now - moveSince > SLOW_MOVE_PERIOD then
+                if myo.getXDirection()== "towardElbow" then
+                    relativePitch=-relativePitch;
+                end
+                if relativePitch>0 then
+                    moveDown()
+                else
+                    moveUp()
+                end    
+                moveSince = now
+            end
+        end
+        
+        local relativeRoll = degreeDiff(getMyoRollDegrees(), rollReference)
+        relativeRoll=conditionalRoll(relativeRoll)
+        if math.abs(relativeRoll)> ROLL_MOTION_THRESHOLD then
+            if now - moveSince > SLOW_MOVE_PERIOD then
+                if relativeRoll>0 then
+                    zoomIn()
+                else
+                    zoomOut()
+                end    
+                moveSince = now
+            end
+        end
+        
+    end
 end
 
 -- Only activate when using SolidWorks
 function onForegroundWindowChange(app, title)
-	enabled = true
+    enabled = true
     if string.match(title, "SolidWorks") then
-		return true
-	end
+        return true
+    end
 end
